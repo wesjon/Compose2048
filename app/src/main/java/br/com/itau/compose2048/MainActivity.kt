d3db8593db43da6ef9bbdf688c2ef14b0b30bfd1
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-03-20 21:02:27 -0300

    Add experimental notation

--- app/src/main/java/br/com/itau/compose2048/MainActivity.kt
+++ app/src/main/java/br/com/itau/compose2048/MainActivity.kt
@@ -3,12 +3,14 @@ package br.com.itau.compose2048
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Surface
 import br.com.itau.compose2048.theme.Compose2048Theme
 import br.com.itau.compose2048.ui.Board2048
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
